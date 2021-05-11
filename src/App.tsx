c0e1122356ce14f028895bf7d63b9a3b3ee0112b
Kenneth Udovic <kennyudovic@gmail.com>
2021-05-11 09:35:54 -0400

    Basic Camera Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,22 @@
 import { useMemo, useRef, Suspense, useEffect, useState } from 'react'
 import { Canvas, useThree } from '@react-three/fiber'
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
 import Box from './components/Box'
 import OBJ from './components/OBJ'
 import HilbertCurve from './components/HilbertCurve'
 
 import { useDrag } from './hooks/useDrag';
-import OrbitControls2 from './components/camera/OrbitControls';
-
+import OrbitControls from './components/camera/OrbitControls';
 
 import { Vector3 } from 'three';
 
 import './App.css';
 
-function Camera() {
-  return (
-    < >
-      <PerspectiveCamera makeDefault position={[0, 0, 10]} />
-      <OrbitControls />
-    </>
-  )
-}
 
 type Vertices = [number, number, number][];
 
 function DrawCurvesTool(props: { vertices: Vertices }) {
   const [vertices, setVertices] = useState(props.vertices);
-  const positions = useMemo(() => new Float32Array(([] as number[]).concat.apply([], vertices)), [vertices])
+  const positions = useMemo(() => new Float32Array(([] as number[]).concat(...vertices)), [vertices])
   // const positionArr = useMemo(() => new Vector3().fromArray(props.position as number[]), [props]);
 
   const lineRef = useRef<THREE.Line>(null!)
@@ -113,9 +103,7 @@ export default function App() {
     <Canvas 
       gl={{ powerPreference: "high-performance", antialias: true }}
     >
-      {/* <Camera /> */}
-
-      <OrbitControls2>
+      <OrbitControls>
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
         <pointLight position={[-10, -10, -10]} />
@@ -134,7 +122,7 @@ export default function App() {
           <UseDragTool />
           <OBJ objUrl={'http://127.0.0.1:8080/obj/FJ1253_BP50293_FMA59764_Mandibular%20gingiva.obj'}/>
         </Suspense>
-      </OrbitControls2>
+      </OrbitControls>
 
     </Canvas>
   )
---
+++ src/components/BasicCamera.tsx
@@ -0,0 +1,10 @@
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
+
+export default function Camera() {
+  return (
+    < >
+      <PerspectiveCamera makeDefault position={[0, 0, 10]} />
+      <OrbitControls />
+    </>
+  )
+}
