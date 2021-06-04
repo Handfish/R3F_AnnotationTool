50c6f62a0cbfd3d3fd6bdab60ab890a20dca3700
Kenneth Udovic <kennyudovic@gmail.com>
2021-06-04 13:57:00 -0400

    Container Div classes

--- src/App.tsx
+++ src/App.tsx
@@ -227,30 +227,33 @@ function RaycasterInfo () {
 
 export default function App() {
   return (
-    <Canvas 
-      gl={{ powerPreference: "high-performance", antialias: true }}
-    >
-      <RaycasterInfo />
-      <BasicCamera />
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      
-      {/* <HilbertCurve /> */}
-      
-      <Suspense fallback={null}>
-        <Box position={[-1, 0, 0]} />
-        <Box position={[1, 0, 0]} />
-      
-        {/* <Hotspot position={[0, 0, 0]}></Hotspot> */}
-        {/* <OBJ objUrl={'http://127.0.0.1:8080/obj/testBox.obj'}/> */}
 
-        {/* <OBJ objUrl={'http://127.0.0.1:8080/obj/FJ1252_BP50280_FMA59763_Maxillary%20gingiva.obj'}/> */}
-        <DrawCurveTool />
-        {/* <CurvesArray /> */}
-        <OBJ objUrl={'http://127.0.0.1:8080/obj/FJ1253_BP50293_FMA59764_Mandibular%20gingiva.obj'}/>
-      </Suspense>
+    <div className={'fs-container'}>
+      <Canvas 
+        gl={{ powerPreference: "high-performance", antialias: true }}
+      >
+        <RaycasterInfo />
+        <BasicCamera />
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        
+        {/* <HilbertCurve /> */}
+        
+        <Suspense fallback={null}>
+          <Box position={[-1, 0, 0]} />
+          <Box position={[1, 0, 0]} />
+        
+          {/* <Hotspot position={[0, 0, 0]}></Hotspot> */}
+          {/* <OBJ objUrl={'http://127.0.0.1:8080/obj/testBox.obj'}/> */}
 
-    </Canvas>
+          {/* <OBJ objUrl={'http://127.0.0.1:8080/obj/FJ1252_BP50280_FMA59763_Maxillary%20gingiva.obj'}/> */}
+          <DrawCurveTool />
+          {/* <CurvesArray /> */}
+          <OBJ objUrl={'http://127.0.0.1:8080/obj/FJ1253_BP50293_FMA59764_Mandibular%20gingiva.obj'}/>
+        </Suspense>
+
+      </Canvas>
+    </div>
   )
 }
