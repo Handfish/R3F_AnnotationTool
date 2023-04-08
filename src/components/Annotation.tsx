789a5a0144538c4d22a9aa0470d263f11eb23d94
Kenneth Udovic <kennyudovic@gmail.com>
2023-04-08 08:29:59 -0400

    chore: commit various pending changes from years ago

--- src/components/Annotation.tsx
+++ src/components/Annotation.tsx
@@ -1,13 +1,13 @@
 import { Html } from '@react-three/drei'
-import type { MeshProps,  AnnotationProps } from '../@types/custom-typings';
+import type { MeshProps, AnnotationProps } from '../@types/custom-typings';
 
-export default function Annotation (props: MeshProps & AnnotationProps) {
+export default function Annotation(props: MeshProps & AnnotationProps) {
   return (
-    <Html position={props.position} style={{ width:'260px', pointerEvents: 'none' }}>
+    <Html position={props.position} style={{ width: '260px', pointerEvents: 'none' }}>
       <div className='annotation-content' hidden={!props.active}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt ut labore et dolore  
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation  
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
       </div>
     </Html>
   );
--- src/components/BasicCamera.tsx
+++ src/components/BasicCamera.tsx
@@ -5,9 +5,9 @@ export default function Camera() {
   const rotateSpeed = useOrbitSpeedStore(state => state.speed);
 
   return (
-    < >
+    <>
       <PerspectiveCamera makeDefault position={[0, 0, 10]} />
-      <OrbitControls rotateSpeed={rotateSpeed} dampingFactor={0.1}/>
+      <OrbitControls rotateSpeed={rotateSpeed} dampingFactor={0.1} />
     </>
   )
 }
